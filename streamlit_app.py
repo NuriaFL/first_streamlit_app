f9095df70b678db08cef100261be50f3d1fcfae4
NuriaFL <104454234+NuriaFL@users.noreply.github.com>
2022-04-28 14:47:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def get_fruityvice_data(this_fruit_choice): # use REQUESTS lib
     return fruityvice_normalized
 
 def get_fruit_load_list():
-    with my_cnx.cursor() ad my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
