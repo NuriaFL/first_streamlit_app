e93efaeee2968d500ae405bfc3885e95083964e4
NuriaFL <104454234+NuriaFL@users.noreply.github.com>
2022-04-28 14:24:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 # STREAMLIT
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My new Healthy diner')
 
 streamlit.header('Breakfast Menu')
@@ -10,32 +15,28 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# PANDAS
-import pandas
+# use PANDAS lib 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # put a pick list with some fruit preselected
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
 
-#REQUESTS
 streamlit.header('Fruityvice Fruit Advice!')
 
-import requests
+# use REQUESTS lib
 fruit_choice = streamlit.text_input('What fruit would you like information about?' ,'kiwi')
 streamlit.write('The user entered',fruit_choice)
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
-#SNOWFLAKE
-import snowflake.connector
+# don't run anything past here while we troubleshoot
+streamlit.stop()
 
+# use SNOWFLAKE 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute ("select current_user(), current_account(), current_region()")
@@ -46,4 +47,4 @@ streamlit.dataframe(my_data_rows)
                                      
 #import requests
 add_my_fruit = streamlit.text_input('What fruit would you like to add?' ,'jackfruit')
-streamlit.write('The user entered',add_my_fruit)
+streamlit.write('Thanks for adding ',add_my_fruit)
