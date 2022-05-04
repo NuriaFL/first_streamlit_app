70fbe001dc5043b275afc7e16145621716dcdb21
NuriaFL <104454234+NuriaFL@users.noreply.github.com>
2022-05-04 13:40:51 +0200

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,16 +1,6 @@
-import snowflake.connector
+import streamlit
+#import snowflake.connector
 
-# Gets the version
-ctx = snowflake.connector.connect(
-    user='nuriafl',
-    password='Snownfl08',
-    account='NL57057'
-    )
-cs = ctx.cursor()
-try:
-    cs.execute("SELECT current_version()")
-    one_row = cs.fetchone()
-    print(one_row[0])
-finally:
-    cs.close()
-ctx.close()
+streamlit.title('My snowflake connector')
+
+
